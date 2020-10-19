29943f7ad9679331137058f3292f0857d3472084
Roy Wang <falin.wang@outlook.com>
2020-10-18 20:19:45 -0400

    Updates

---
+++ precache-manifest.f4d5df0aa2c10a38dce65bc26832e5ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6591210d71a0bd50d75e7a579fe56ac6",
+    "url": "/cats-rolodex/index.html"
+  },
+  {
+    "revision": "3889795eef3fca7e2ddc",
+    "url": "/cats-rolodex/static/css/main.1e117505.chunk.css"
+  },
+  {
+    "revision": "2a76d07078335a6d5e73",
+    "url": "/cats-rolodex/static/js/2.66c4e969.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/cats-rolodex/static/js/2.66c4e969.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3889795eef3fca7e2ddc",
+    "url": "/cats-rolodex/static/js/main.6e93646f.chunk.js"
+  },
+  {
+    "revision": "3a501412eaa0931833b1",
+    "url": "/cats-rolodex/static/js/runtime-main.a7c11a12.js"
+  }
+]);
